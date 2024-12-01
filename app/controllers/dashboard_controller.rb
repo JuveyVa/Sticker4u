55e4206bac53c538587e5ebc9735a556df1747f5
DarielMorua <12119l@ulsachihuahua.edu.mx>
2024-12-01 00:16:42 -0600

    Se corrige controller e index del dashboard al igual que se crea correctamente el index principal

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,12 +1,12 @@
 class DashboardController < ApplicationController
-  protect_from_forgery with: :null_session # Si hay problemas con CSRF
+  protect_from_forgery with: :null_session 
 
   def index
     @products = Product.all
     @tickets = Ticket.all
     @categories = Categoria.all
   
-    # Datos iniciales para las gráficas
+    # Datos iniciales
     @categories_data = @categories.map { |category| [category.categoria, category.products.count] }
     @tickets_data = @tickets.map { |ticket| [ticket.date.strftime('%Y-%m-%d'), ticket.total] }
   end
@@ -16,20 +16,32 @@ class DashboardController < ApplicationController
     selected_tickets = params[:selected_tickets] || []
     selected_categories = params[:selected_categories] || []
   
-    # Filtrar datos de tickets
+    # se filtran datos de tickets
     tickets_data = Ticket.where(:_id.in => selected_tickets.map { |id| BSON::ObjectId(id) })
                          .map { |ticket| [ticket.date.strftime('%Y-%m-%d'), ticket.total] }
-  
-    # Filtrar datos de categorías
+
+    # se filtran datos de categorías
     categories_data = Categoria.where(:_id.in => selected_categories.map { |id| BSON::ObjectId(id) })
                                .map { |category| [category.categoria, category.products.count] }
-  
-    # Enviar datos al cliente
+
+    # se filtran productos por categorías seleccionadas
+    if selected_categories.present?
+      @products = Product.where(categoria_id: selected_categories.map { |id| BSON::ObjectId(id) })
+    else
+      @products = Product.all
+    end
+
+    # agrupamos los productos por fecha de creacion
+    products_line_data = @products.group_by { |product| product.created_at.strftime("%Y-%m-%d") }
+                                  .map { |date, products| [date, products.count] }
+
+    # definimos la variable tickets_scatter_data 
+    tickets_scatter_data = tickets_data.map { |ticket| { date: ticket[0], total: ticket[1] } }
+
     render json: {
-      categories_pie_data: categories_data,
-      tickets_scatter_data: tickets_data,
-      categories_bar_data: categories_data
+      categories_pie_data: categories_data,  # Pie chart data
+      tickets_scatter_data: tickets_scatter_data,  # Scatter chart data
+      products_line_data: products_line_data  # Line chart data
     }
   end
-  
 end
