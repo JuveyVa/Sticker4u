1ef8bb886404187744ba5d42c0a160c29cfaa87f
Luis Ivan Jasso Lopez <11995@ulsachihuahua.edu.mx>
2024-12-01 01:05:54 -0600

    Se crea pantalla de predicciones junto con la logica para poder realizar predicciones en base a los tickets y productos

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -4,4 +4,8 @@ class Ticket
   field :date, type: Date
   field :total, type: Float
   has_and_belongs_to_many :products
+
+  def product_frequency
+    products.group_by(&:id).transform_values(&:size)
+  end
 end
